1b3043abb7c0211743201e057602fbac0ec4e5f7
Jakub <jochlik@email.cz>
2020-11-22 16:29:52 +0100

    changed bank data loading

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def get_last_4_months():
     today = datetime.datetime.today()
 
     bank_url = BANK_BASE_URL + str(today.year)
-    bank_currency_data = requests.get(url=bank_url).text
+    bank_currency_data = [requests.get(url=bank_url).text]
 
     if today.month < LAST_4_MONTHS:  # handle situation, when months from last year are needed
         bank_url = BANK_BASE_URL + str(today.year - 1)
         result = requests.get(url=bank_url).text
-        bank_currency_data = result + "\n".join(bank_currency_data.split("\n")[1:])  # remove header from second GET call so we dont have it twice in our data
+        bank_currency_data.append(result)  # remove header from second GET call so we dont have it twice in our data
 
     return bank_currency_data
 
@@ -40,47 +40,49 @@ def get_4_months(date):
     """
     :param date: tells me which months are being requested in format [month, year] as strings
     """
-    today = datetime.datetime.today()
 
     bank_url = BANK_BASE_URL + str(date[1])
-    bank_currency_data = requests.get(url=bank_url).text
+    bank_currency_data = [requests.get(url=bank_url).text]
 
-    if int(date[0]) >= 9 and int(date[1]) + 1 <= today.year:
+    if int(date[0]) < LAST_4_MONTHS:
         bank_url = BANK_BASE_URL + str(int(date[1]) + 1)
         result = requests.get(url=bank_url).text
-        bank_currency_data = bank_currency_data + "\n".join(
-            result.split("\n")[1:])  # remove header from second GET call so we dont have it twice in our
+        bank_currency_data.append(result)  # remove header from second GET call so we dont have it twice in our
 
     return bank_currency_data
 
 def transform_text_data_to_dictionary(data):
     """
     transforms text file data from bank server to dictionary(json), which is required by monogo database
-    :param data: in form of pure text
+    :param data: in form of pure text in list
     :return: dictionary containing all data
     """
 
+    processed_data = []
+    for currency_year in data:
+        for new_block in ["Datum" + block for block in currency_year.split("Datum") if block]:
+            processed_data.append(new_block)
+
     dict_data = {}
-    currency_list = []
-    currency_array_tmp = data.split('\n')[0].split('|')[1:]
 
-    for currency in currency_array_tmp:
-        currency_info = currency.split(' ')
-        currency_list.append(currency_info[1])
-        dict_data[currency_info[1]] = [('amount', currency_info[0])]
+    for currency_block in processed_data:
+        currency_array_tmp = currency_block.split('\n')[0].split('|')[1:]
 
-    for key in dict_data:
-        key_index = 0
-        for currency_column in bank_data.split('\n')[0].split('|'):
-            if key in currency_column:
-                for line in bank_data.split('\n')[1:-1]:
-                    if "Datum" in line: # for some reason, for some months bank does not provide data for same currencies... skip those data
-                        return dict_data
-                    line_split = line.split('|')
-                    dict_data[key].append((line_split[0], line_split[key_index]))
-                break
-            else:
-                key_index = key_index + 1
+        for currency in currency_array_tmp:
+            currency_info = currency.split(' ')
+            if currency_info[1] not in dict_data:
+                dict_data[currency_info[1]] = [('amount', currency_info[0])]
+
+        for key in dict_data:
+            key_index = 0
+            for currency_column in currency_block.split('\n')[0].split('|'):
+                if key in currency_column:
+                    for line in currency_block.split('\n')[1:-1]:
+                        line_split = line.split('|')
+                        dict_data[key].append((line_split[0], line_split[key_index]))
+                    break
+                else:
+                    key_index = key_index + 1
 
     return dict_data
 
